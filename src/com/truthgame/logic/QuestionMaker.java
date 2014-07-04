5dd8dc6315824f0d1b8a604ba18a4e56d854dfa0
Michael M <mihael.muhayluk@gmail.com>
2014-07-04 21:38:52 +0300

    added questions

--- src/com/truthgame/logic/QuestionMaker.java
+++ src/com/truthgame/logic/QuestionMaker.java
@@ -6,10 +6,10 @@ import android.app.Activity;
 public class QuestionMaker {
 	
 	private Activity activity;
-	private CharSequence[] red;
-	private CharSequence[] blue;
-	private CharSequence[] yellow;
-	private CharSequence[] white;
+	private CharSequence[] redWords;
+	private CharSequence[] blueWords;
+	private CharSequence[] yellowWords;
+	private CharSequence[] whiteWords;
 	
 	public QuestionMaker(Activity activity) {
 		this.activity = activity;		
